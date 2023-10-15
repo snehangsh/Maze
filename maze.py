be597081178d11eb6641ea0a260b63013c7628bb
snehangsh <snehangshubarmon943@gmail.com>
2023-10-15 14:46:08 +0530

    breaking walls

--- maze.py
+++ maze.py
@@ -3,7 +3,7 @@ import random
 import time
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -13,6 +13,7 @@ class Maze:
         self._cell_size_y = cell_size_y
         self._win = win
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -38,7 +39,11 @@ class Maze:
         if self._win is None:
             return 
         self._win.redraw()
-        time.sleep(0.05)
-
-
+        time.sleep(0.02)
+        
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
 
